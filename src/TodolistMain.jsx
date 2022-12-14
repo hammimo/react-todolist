2412e02d88c663f92b39cb2d7d52122537f46d7f
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-14 20:43:17 +0900

    add

---
+++ src/TodolistMain.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Main.css';
+
+export default function TodolistMain() {
+    return (
+        <div className={styles.container}>
+            <header className='navbar'></header>
+            <section className='main'></section>
+            <footer className='foobar'></footer>
+        </div>
+    );
+}
+

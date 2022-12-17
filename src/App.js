d6a63c7a45eccea71857906d1662d02350ec9534
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-18 07:19:21 +0900

    Provider 위 빈 <> 제거

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
       <DarkModeProvider>
         <Header filters={filters} filter={filter} onFilterChange={setFilter}/>
         <TodoList filter={filter}/>
       </DarkModeProvider>
-    </>
   );
 }
 

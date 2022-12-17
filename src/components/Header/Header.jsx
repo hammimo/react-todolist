d765b90bff10683e0a50f8ad4ef4871a321668b3
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-18 07:50:02 +0900

    DarkMode CSS
    Tailwind 사용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { HiMoon, HiSun } from 'react-icons/hi';
 export default function Header({filters, filter, onFilterChange}) {
     const {darkMode, toggleDarkMode} = useDarkMode();
     return <header className={styles.header}>
-        <button onClick={toggleDarkMode}>
+        <button onClick={toggleDarkMode} className={styles.toggle}>
             {!darkMode && <HiMoon/>}
             {darkMode && <HiSun/>}
         </button>

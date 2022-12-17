f52bb328f7156cc14a2c9eab5defd9169b74d715
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-12-18 07:35:19 +0900

    DarkModeContext완성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
-import { DarkModeProvider } from './components/DarkModeContext';
+import { DarkModeProvider } from './components/context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 function App() {
--- src/components/context/DarkModeContext.jsx
+++ src/components/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export const DarkModeContext = createContext(); 
 
@@ -7,7 +7,16 @@ export function DarkModeProvider({children}){
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
         updateDarkMode(!darkMode);
-    }
+    };
+
+    useEffect(() =>{
+        const isDark =
+        localStorage.theme === 'dark' || 
+        (!('theme' in localStorage) && 
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+        setDarkMode(isDark);
+        updateDarkMode(isDark);
+    }, []);
 
     return <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
         {children}</DarkModeContext.Provider>
@@ -16,8 +25,10 @@ export function DarkModeProvider({children}){
 function updateDarkMode(darkMode){
     if(darkMode){
         document.documentElement.classList.add('dark');
+        localStorage.theme = 'dark';
     } else {
         document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 }
 export const useDarkMode = () => useContext(DarkModeContext);
